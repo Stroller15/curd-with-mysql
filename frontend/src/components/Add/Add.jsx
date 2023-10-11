599e2c89b4cf0c4b2b35156c97308e8fe7a3f57e
shubham verma <shubhamverma@shubhams-MacBook-Air.local>
2023-10-11 17:21:26 +0530

    add new data from react UI

--- frontend/src/components/Add/Add.jsx
+++ frontend/src/components/Add/Add.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Add = () => {
+  const [book, setBook] = useState({
+    title: '',
+    desc: '',
+    price: null,
+    cover: ''
+
+  })
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) =>  ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/books", book);
+      navigate('/');
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  console.log(book)
+
   return (
-    <div>Add</div>
+    <div className='form'>
+      <h1>Add new books in library</h1>
+      <input type="text"  name='title' placeholder='title' onChange={handleChange}/>
+      <input type="text" name='desc' placeholder='desc' onChange={handleChange}/>
+      <input type="number" name='price' placeholder='price' onChange={handleChange}/>
+      <input type="text" name='cover' placeholder='cover' onChange={handleChange}/>
+      <button onClick={handleClick}>Add to DB</button>
+    </div>
   )
 }
 
