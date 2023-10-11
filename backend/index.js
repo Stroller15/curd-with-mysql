9269adf2ca7e7d4a700b4f0533604d2593fa7794
shubham verma <shubhamverma@shubhams-MacBook-Air.local>
2023-10-11 10:21:07 +0530

    implement post api

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import  express  from "express";
 import mysql from "mysql";
 
 
-
 const app = express();
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -35,6 +35,17 @@ app.get('/books', (req, res) => {
         return res.json(data);
     })
 })
+
+app.post('/books', (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+    const values = [req.body.title, req.body.desc, req.body.cover];
+    db.query(q, [values], (err, data) => {
+        if(err) return res.json(err);
+        return res.json("book have been created");
+    })
+})
+
+
 app.listen(5000, () => {
     console.log('server is running on port 5000')
     
